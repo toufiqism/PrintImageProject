22c047dcf1de08313106ea3e2021f4a8fc38f3e6
Fahad Bin Abedin <fbabedin@ael-bd.com>
2016-05-16 17:27:47 +0600

    ui update

--- ImagePrinting/app/src/main/java/com/example/sabbirhossain/imageprinting/SelectPrintDocumentListActivity.java
+++ ImagePrinting/app/src/main/java/com/example/sabbirhossain/imageprinting/SelectPrintDocumentListActivity.java
@@ -3,11 +3,15 @@ package com.example.sabbirhossain.imageprinting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SelectPrintDocumentListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_select_print_document_list);
+
+
+
+
     }
 }
